0e3c7081e9f2b58df516e3c87eececb2fffebbf4
Ethan Miller <ethan.romans5.8@gmail.com>
2019-06-28 22:48:56 -0500

    get user input

--- Test.java
+++ Test.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Test {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Print text to console.
+    System.out.println("What is your name?");
+
+    // Get user input.
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+
+    // Concatenate string with input and print to console.
+    System.out.println("Hello " + name);
   }
   
 }
